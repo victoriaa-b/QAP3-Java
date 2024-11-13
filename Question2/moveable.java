fd35f8f05cb270790af02e224706aeacc26bb490
Victoria <162462385+victoriaa-b@users.noreply.github.com>
2024-11-13 15:26:47 -0330

    Update moveable.java

--- Question2/moveable.java
+++ Question2/moveable.java
@@ -1,5 +1,63 @@
 package Question2;
 
-public class moveable {
-    
+public class movablePoint extends point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    // constructors
+    public movablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y); // take parent 
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public movablePoint(float xSpeed, float ySpeed) {
+        super(0.0f, 0.0f); 
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public movablePoint() {
+        super(0.0f, 0.0f);
+    }
+
+    // getters and setters
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public float[] getSpeed() {
+        return new float[] { xSpeed, ySpeed };
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    // toString to display the x and y coordinates of the point and speed
+    @Override
+    public String toString() {
+        return String.format("%s, Speed: (xSpeed=%.1f, ySpeed=%.1f)", super.toString(), xSpeed, ySpeed);
+    }
+
+    // needed in order to update position
+    public movablePoint move() {
+        setX(getX() + xSpeed);
+        setY(getY() + ySpeed);
+        return this;
+    }
 }
+
